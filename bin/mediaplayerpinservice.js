416061fa0a2c530d6ecc6967b5dfe6886be7dd56
phoyle <pete.hoyle@genesys.com>
2019-07-29 10:58:23 +0100

    Initial commit

---
+++ bin/mediaplayerpinservice.js
@@ -0,0 +1,62 @@
+//var io = require('socket.io')(8088,  ({path: '/pete/test'},{host:'0.0.0.0'}));
+
+///Class to monitor the pin changes made by MediaServer
+///When it receives a 'save' message from a MediaServer it will broadcast the message to all registered Mediaservers
+
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http,  ({path: '/PinServiceManager1/webapi/broadcast'}));
+
+var pins = '{}';
+var fs = require('fs');
+
+
+http.listen(8088, '0.0.0.0', function(){
+    console.log('ServerAdapter: ' + io.adapter());
+});
+
+io.on('connection', function (socket) {
+    console.log('Connected to client');
+    try {
+        var test = fs.readFileSync('pins.json');
+        pins = JSON.parse(test);
+    }
+    catch (e) {
+
+    }
+    sendMessage(socket,'pins',pins);
+    socket.on('message', function (message) {
+        console.log('Message: ' + message);
+        switch (message.status) {
+            case 'test':
+                console.log('You sent test');
+                break;
+            case 'save':
+                console.log("Save the pins: ");
+        }
+    });
+
+    socket.on('save', function (message) {
+        console.log('Save Pins: ' + message);
+        this.pins = JSON.parse(message);
+        fs.writeFileSync('pins.json',message);
+        broadcastMessage(socket,'pins',this.pins);
+    });
+
+    socket.on('disconnect', function (data) {
+        console.log('disconnected from client: ' + data);
+    });
+
+
+});
+
+function broadcastMessage(socket, action, text) {
+    console.log('Broadcast Action: ' + action + ' text: ' + text);
+    socket.broadcast.emit(action, text);
+}
+
+
+function sendMessage(socket, action, text) {
+    console.log('Send Action: ' + action + ' text: ' + text);
+    socket.emit(action, text);
+}
